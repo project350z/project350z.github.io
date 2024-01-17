878e802fcfade61ad4782128044aba9710c661eb
ChrisHammond <ChrisHammond@users.noreply.github.com>
2024-01-17 00:39:24 +0000

    deploy: af6d2dbbb94dab807d75059346ae5bfc7f8f9518

---
+++ updateredirect_to.ps1
@@ -0,0 +1,49 @@
+$directoryPath = ".\_posts\*"
+
+# Get all .html and .md files
+$files = Get-ChildItem -Path $directoryPath -Include "*.html", "*.md" -Recurse
+
+foreach ($file in $files) {
+    Write-Host "Processing file: $($file.FullName)"
+    $content = Get-Content $file.FullName -Raw
+
+    # Adjusted regex for more flexibility with line endings and spaces
+    $yamlPattern = "(?s)^\s*---\s*(.+?)\s*---"
+
+    # Check if file contains YAML front matter
+    if ($content -match $yamlPattern) {
+        $yaml = $matches[1]
+        Write-Host "Found YAML front matter."
+
+        # Check for permalink in YAML
+        if ($yaml -match "permalink:\s*(.+)") {
+            $permalink = $matches[1].Trim()
+            # Ensure permalink starts with a '/'
+            if (-not $permalink.StartsWith("/")) {
+                $permalink = "/" + $permalink
+            }
+            $redirectUrl = "https://www.autocrossblog.com$permalink"
+            Write-Host "Found permalink: $permalink"
+
+            # Add or update redirect_to in YAML
+            if ($yaml -notmatch "redirect_to:") {
+                $yaml = $yaml + "`nredirect_to:
+  - $redirectUrl"
+                Write-Host "Added redirect_to."
+            } else {
+                $yaml = $yaml -replace "(?<=redirect_to:).*", " $redirectUrl"
+                Write-Host "Updated redirect_to."
+            }
+
+            # Replace old YAML with new YAML
+            $newContent = $content -replace $yamlPattern, "---`n$yaml`n---"
+
+            # Write the new content back to the file
+            Set-Content -Path $file.FullName -Value $newContent
+        } else {
+            Write-Host "No permalink found in YAML."
+        }
+    } else {
+        Write-Host "No YAML front matter found."
+    }
+}
