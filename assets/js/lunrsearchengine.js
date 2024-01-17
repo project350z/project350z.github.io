87a0d1a474f2b8990c2ff3ef82209115c79dbb07
ChrisHammond <ChrisHammond@users.noreply.github.com>
2024-01-17 23:36:23 +0000

    deploy: dcd7acc44fd2f90250e58ef4c520c07cff17ae60

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "/about",
     "title": "About Project350z.com",
-    "body": "Project350z. com was originally about a boy and his toy. The first was a 2003 350z Enthusiast Package. That car was campaigned for 3 years and then sold to a Dozzle in Kansas City (he still owns it and won’t sell it back). The second was a 2004 Enthusiast. Picked up in Colorado in January 2010, it travelled to California and spent 2 years there before being sold. The third was a 2003 Touring that was acquired in 2006. That only stuck around 2 years before making its way to Florida, and then recently on to Colorado. What will the 4th be? Who knows. The current car can be found over on SpecFRS. com "
+    "body": "Project350z. com was originally about a boy and his toy. The first was a 2003 350z Enthusiast Package. That car was campaigned for 3 years and then sold to a Dozzle in Kansas City (he still owns it and won’t sell it back). The second was a 2004 Enthusiast. Picked up in Colorado in January 2010, it travelled to California and spent 2 years there before being sold. The third was a 2003 Touring that was acquired in 2016. That only stuck around 2 years before making its way to Florida, and then recently on to Colorado. What will the 4th be? Who knows. The current car can be found over on SpecFRS. com "
     }, {
     "id": 2,
     "url": "/carsetup",
